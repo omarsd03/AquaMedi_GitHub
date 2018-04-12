16195fb69d0bbc22d86bc00b807ed34d69409450
omarsd03 <34853959+omarsd03@users.noreply.github.com>
2018-04-12 16:52:11 -0500

    Primer

---
+++ view/inicio.view.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<link href='img/AquaMediHorizontal.png' rel='shortcut icon' type='image/png'>
+	<title>Inicio</title>
+	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<link rel="stylesheet" href="css/estilos_paginas.css">
+	<link rel="stylesheet" href="css/estilos_menu.css">
+	<link rel="stylesheet" href="css/bootstrap/bootstrap.css">
+	<link rel="stylesheet" href="icomoon/style.css">
+</head>
+<body>
+
+	<?php include 'extras/header.php'; ?>
+
+	<div class="container" align="center">
+		<h1>Bienvenido</h1>
+	</div>
+
+	<div class="container">
+		<section class="main row">
+			<article class="col-xs-12 col-sm-8 col-md-8 col-lg-8">
+				<h2>Manejo de Fluido de Agua</h2>
+				<p>
+					Sitio web capaz de llevar un monitoreo del consumo de agua en tiempo real, proporcionando informacion de cada uno de los municipios mas consumistas del recurso.
+				</p>
+			</article>
+
+			<aside class="col-xs-12 col-sm-4 col-md-4 col-lg-4">
+				<h3>Sabias que....</h3>
+				<p>
+					Las plantas deben ser regadas por las mañanas o por la tarde noche, ya que si se riegan durante las horas calurosas el agua se evapora y requerira mas agua de lo normal
+				</p>
+			</aside>
+		</section>
+
+		<div class="row">
+			<div class="color1 col-xs-12 col-sm-6 col-md-3">
+				<h3>Aqui van los analitics</h3>
+				<p>
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus minima nulla quasi porro sunt suscipit quibusdam velit itaque voluptates soluta, quaerat. Quisquam rerum enim nulla quia, sapiente magnam quos ad.
+				</p>
+			</div>
+			<div class="col-xs-12 col-sm-6 col-md-3">
+				<h3>Aqui van los analitics</h3>
+				<p>
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur aliquid dolor voluptatum, nulla impedit hic aliquam beatae tempora, id enim, perspiciatis incidunt necessitatibus voluptas. Ipsum ducimus, veritatis laboriosam eveniet ipsa?
+				</p>
+			</div>
+
+			<div class="clearfix visible-sm-block"></div> <!--Este div limpia todos los errores de posicion en los demas divs/-->
+
+			<div class="color1 col-xs-12 col-sm-6 col-md-3">
+				<h3>Aqui van los analitics</h3>
+				<p>
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum veniam voluptates ipsum voluptatum, magni repudiandae, dolorum quisquam, voluptatem et reiciendis quos sit officia doloribus nulla voluptate architecto! Eos, voluptatum, iusto?
+				</p>
+			</div>
+			<div class="col-xs-12 col-sm-6 col-md-3">
+				<h3>Aqui van los analitics</h3>
+				<p>
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur dolore numquam, eaque esse minima aut, architecto doloribus. Vitae beatae, temporibus voluptatibus ea, ad similique perspiciatis sed doloribus, explicabo error asperiores.
+				</p>
+			</div>
+		</div>
+	</div>
+
+	<?php include 'extras/footer.php'; ?>
+
+	<script src="js/jquery-3.2.1.min.js"></script>
+	<script src="js/menu.js"></script>
+	<script src="js/main.js"></script>
+	<script src="http://code.jquery.com/jquery-latest.js"></script>
+</body>
+</html>
